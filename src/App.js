f71e69d76348496fbb0c76933d2eeb34485b2168
HandyAndy <andreasocross@yahoo.com>
2022-05-17 14:04:44 +0200

    add props for products

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import CountButton from './CountButton/countbutton'
 import SearchBar from './SearchBar/SearchBar'
+
+const products = [
+   'shampoo',
+   'body wash',
+   'face wash',
+   'soap bar',
+   'conditioner'
+]
 
 const App = () => {
     return (
      <div>
-        <CountButton incrementBy={5} buttonColour = {"blue"} />
-        <CountButton incrementBy={2} buttonColour = {"purple"}/>
-        <SearchBar />
+        <SearchBar products={products}/>
      </div>
     )  
   }
